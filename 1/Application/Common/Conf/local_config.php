00fa9d615ec124de1fcc7a149fa2811e4398ab83
zjien <694396727@qq.com>
2015-02-01 00:02:19 +0800

    修改配置代码

--- 1/Application/Common/Conf/local_config.php
+++ 1/Application/Common/Conf/local_config.php
@@ -2,11 +2,13 @@
 return array(
     //请在该文件中配置数据库信息
 		'DB_TYPE'           => 'mysql',
-		'DB_HOST'           => '10.9.1.188', //coding上的mysql主机名
+		'DB_HOST'           => 'localhost', //coding上的mysql主机名
 		'DB_PORT'           =>  3306,        //端口
-		'DB_NAME'           => 'cf_cc84a906_82a5_4e13_8fd4_59ce4949379e',    //数据库名称
+		'DB_NAME'           => 'tosport',    //数据库名称
 		'DB_CHARSET'        => 'utf8',       //字符集
 		'DB_PREFIX'         => 'spt_',       //表前缀
-        'DB_USER'			=> 'TAvIZdCoKpT1B5V9',	//默认数据库用户名
-        'DB_PWD'			=> 'qI2FK7d9oXUvmZc7'	//数据库密码  
+        'DB_USER'			=> 'root',	//默认数据库用户名
+        'DB_PWD'			=> '123456'	//数据库密码  
 );
+
+
