56f8395f986537d87a1d26c410f6480e2c6e3d93
zjien <694396727@qq.com>
2015-01-31 23:57:55 +0800

    配置文件的修改

---
+++ 1/Application/Common/Conf/local_config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    //请在该文件中配置数据库信息
+		'DB_TYPE'           => 'mysql',
+		'DB_HOST'           => '10.9.1.188', //coding上的mysql主机名
+		'DB_PORT'           =>  3306,        //端口
+		'DB_NAME'           => 'cf_cc84a906_82a5_4e13_8fd4_59ce4949379e',    //数据库名称
+		'DB_CHARSET'        => 'utf8',       //字符集
+		'DB_PREFIX'         => 'spt_',       //表前缀
+        'DB_USER'			=> 'TAvIZdCoKpT1B5V9',	//默认数据库用户名
+        'DB_PWD'			=> 'qI2FK7d9oXUvmZc7'	//数据库密码  
+);
