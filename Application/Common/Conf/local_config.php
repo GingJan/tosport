6582694de42a9ba14616cd733f0878211279061b
zjien <694396727@qq.com>
2015-03-16 19:54:02 +0800

    更新数据连接信息

---
+++ Application/Common/Conf/local_config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    //请在该文件中配置数据库信息
+		'DB_TYPE'           => 'mysql',
+		'DB_HOST'           => '192.168.3.5', //coding上的mysql主机名
+		'DB_PORT'           =>  3306,        //端口
+		'DB_NAME'           => 'cf_2b739d5a_6d89_4a2e_b9f3_9319314fae7f',    //数据库名称
+		'DB_CHARSET'        => 'utf8',       //字符集
+		'DB_PREFIX'         => 'spt_',       //表前缀
+        'DB_USER'			=> 'H1azwhnV8x9FppYg',	//默认数据库用户名
+        'DB_PWD'			=> 'v3N6R41LsDNJTni5'	//数据库密码  
+);
+
+
