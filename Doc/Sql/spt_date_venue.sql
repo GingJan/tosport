4e83ddd3be9912fa38f44359a6da723b41f5b2f8
zjien <694396727@qq.com>
2015-03-24 17:24:09 +0800

    修改表spt_date_venue表的ma_id字段

--- Doc/Sql/spt_date_venue.sql
+++ Doc/Sql/spt_date_venue.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2015 年 02 月 06 日 16:02
+-- 生成日期: 2015 年 03 月 24 日 17:23
 -- 服务器版本: 5.5.28
 -- PHP 版本: 5.3.13
 
@@ -28,21 +28,54 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `spt_date_venue` (
   `dv_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增长id',
+  `ma_id` int(10) unsigned DEFAULT NULL COMMENT '创建该场馆的管理员',
   `subscriber` int(10) unsigned NOT NULL COMMENT '预订人的u_id',
   `vi_id` int(10) unsigned NOT NULL COMMENT '场馆的vi_id',
   `date_time` int(10) unsigned NOT NULL COMMENT '预约时间',
   `order_time` int(10) unsigned NOT NULL COMMENT '下单时间',
+  `done` int(10) unsigned DEFAULT NULL COMMENT '是否结单(1代表已结单)',
   PRIMARY KEY (`dv_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='场馆预约表' AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='场馆预约表' AUTO_INCREMENT=6 ;
 
 --
 -- 转存表中的数据 `spt_date_venue`
 --
 
-INSERT INTO `spt_date_venue` (`dv_id`, `subscriber`, `vi_id`, `date_time`, `order_time`) VALUES
-(4, 1, 1, 1423159938, 1423151968),
-(2, 1, 2, 1423146000, 1423145858),
-(3, 2, 4, 1423159938, 1423151959);
+INSERT INTO `spt_date_venue` (`dv_id`, `ma_id`, `subscriber`, `vi_id`, `date_time`, `order_time`, `done`) VALUES
+(1, 2, 1, 1, 1423159938, 1423151968, 1),
+(2, 2, 1, 2, 1423146000, 1423145858, 1),
+(3, 3, 2, 4, 1423159938, 1423151959, NULL),
+(5, 1, 1, 3, 1423327220, 1423326232, NULL);
+
+--
+-- 触发器 `spt_date_venue`
+--
+DROP TRIGGER IF EXISTS `tg_venue_info_booked_up`;
+DELIMITER //
+CREATE TRIGGER `tg_venue_info_booked_up` AFTER INSERT ON `spt_date_venue`
+ FOR EACH ROW begin
+update spt_venue_info set booked=booked+1 where
+vi_id=new.vi_id;
+end
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `tg_venue_info_bought_up`;
+DELIMITER //
+CREATE TRIGGER `tg_venue_info_bought_up` AFTER UPDATE ON `spt_date_venue`
+ FOR EACH ROW begin
+update spt_venue_info set bought=bought+1 where
+vi_id=new.vi_id;
+end
+//
+DELIMITER ;
+DROP TRIGGER IF EXISTS `tg_venue_info_booked_down`;
+DELIMITER //
+CREATE TRIGGER `tg_venue_info_booked_down` AFTER DELETE ON `spt_date_venue`
+ FOR EACH ROW begin
+update spt_venue_info set booked=booked-1 where vi_id=old.vi_id;
+end
+//
+DELIMITER ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
